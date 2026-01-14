1b9467c1d1d838e12daf0a4a7334bf3cd7cfc472
tushar <tmjn0007@gmail.com>
2026-01-14 13:43:07 +0530

    new pages appointment

---
+++ Front/AppointmentMaster.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+
+namespace WSBillingMaster.Pages
+{
+    public partial class AppointmentMaster : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // You can load jobs here later
+        }
+
+    }
+}
