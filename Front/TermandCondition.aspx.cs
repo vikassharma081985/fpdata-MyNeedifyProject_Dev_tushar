d5242beecbd8c3f1b4793c0f4d0d35656c2980a3
tushar <tmjn0007@gmail.com>
2026-01-29 22:17:29 +0530

    t & c page create

---
+++ Front/TermandCondition.aspx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace FaduPrice.Pages
+{
+    public partial class TermandCondition : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
